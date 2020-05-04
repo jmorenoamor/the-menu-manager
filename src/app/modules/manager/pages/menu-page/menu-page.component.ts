378abecce1777e747f0e4a744b0a39e499a344e2
Jesus Moreno Amor <jesus@morenoamor.com>
2020-05-04 11:26:23 +0200

    Models, service, and basic page

--- src/app/modules/manager/pages/menu-page/menu-page.component.ts
+++ src/app/modules/manager/pages/menu-page/menu-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoggingService } from 'src/app/modules/core';
+import { BackendService } from 'src/app/modules/manager/services';
+
+import { ResultList } from 'src/app/modules/core/models';
+import { MenuEntry } from 'src/app/modules/manager/models';
+
 @Component({
   selector: 'app-menu-page',
   templateUrl: './menu-page.component.html',
@@ -7,10 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPageComponent implements OnInit {
 
-  constructor() { }
+  public entries: MenuEntry[];
+
+  constructor(
+    private logger: LoggingService,
+    private backend: BackendService,
+  ) { }
 
   ngOnInit(): void {
-    console.log("OK");
+
+    this.backend.getMenuEntries().subscribe(
+      (data: ResultList<MenuEntry> ) => {
+        this.entries = data.results  ;
+      },
+      (error: any) => {
+        this.logger.error("Error no contemplado.");
+      }
+    );
+
   }
 
 }
--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,8 @@ export const environment = {
   production: false,
   debug: true,
   log_level: 1,
-  profile: true
+  profile: true,
+  backend: {
+    url: 'http://localhost:3000',
+  }
 };
